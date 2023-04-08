f7714afed48b257accf9f23caf1454be78f38855
Amir <amir.gheser@studenti.unitn.it>
2023-04-08 15:32:29 +0200

    Working import and cloned library

---
+++ main.py
@@ -0,0 +1,15 @@
+################################################################################
+# In this file we will implement the main function of the project.
+# @author: Amir Gheser
+################################################################################
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+sys.path.insert(0, '/Users/amirgheser/HG-dismantling/hyperlink-communities')
+import loaders
+import linegraph
+
+print(loaders.load_broken_examples())
+
